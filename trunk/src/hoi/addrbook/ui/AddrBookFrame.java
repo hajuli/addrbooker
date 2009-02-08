c1cd5aa08112a7068a76f82f8e092045e6e8a712
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-02-08 15:08:34 +0000

    demo backup

---
+++ trunk/src/hoi/addrbook/ui/AddrBookFrame.java
@@ -0,0 +1,51 @@
+package hoi.addrbook.ui;
+
+import java.awt.BorderLayout;
+
+import hoi.addrbook.icons.ImageHelper;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class AddrBookFrame extends JFrame {
+
+	private static final long serialVersionUID = -3116155141455555134L;
+
+	public static final String TITLE = "海石通讯录";
+
+	public AddrBookFrame() {
+		try {
+			SwingUtilities.invokeAndWait(new Runnable() {
+				public void run() {
+					setLayout(new BorderLayout());
+					add(new AddrBookPanel(), BorderLayout.CENTER);
+				}
+			});
+		} catch (Exception exc) {
+			System.err.println("Can't create because of " + exc);
+		}
+	}
+
+	private static void setLookAndFeel() {
+		try {
+			com.jtattoo.plaf.acryl.AcrylLookAndFeel.setTheme("Lemmon", "??", "Hoi");
+			UIManager.setLookAndFeel(new com.jtattoo.plaf.acryl.AcrylLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		setLookAndFeel();
+		AddrBookFrame frame = new AddrBookFrame();
+		frame.setSize(600, 450);
+		frame.setLocationRelativeTo(null);
+		frame.setIconImage(ImageHelper.ICON_LOGO.getImage());
+		frame.setTitle(TITLE);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
