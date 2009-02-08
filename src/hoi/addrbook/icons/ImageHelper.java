82e834fb6cc9b11db82d29e694039f6fef94d6cb
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-02-08 15:03:00 +0000

    icons

---
+++ src/hoi/addrbook/icons/ImageHelper.java
@@ -0,0 +1,32 @@
+package hoi.addrbook.icons;
+
+import javax.swing.ImageIcon;
+
+public class ImageHelper {
+
+	private ImageHelper() {
+	}
+
+	/** Returns an ImageIcon, or null if the path was invalid. */
+	public static ImageIcon createImageIcon(String path) {
+		java.net.URL imgURL = ImageHelper.class.getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
+
+	public static final ImageIcon ICON_LOGO = createImageIcon("128/kugar.png");
+
+	public static final ImageIcon ICON_ADD = createImageIcon("32/edit_add.png");
+	public static final ImageIcon ICON_SAVE = createImageIcon("32/filesave.png");
+	public static final ImageIcon ICON_EDIT = createImageIcon("32/Edit.png");
+	public static final ImageIcon ICON_DELETE = createImageIcon("32/stop.png");
+	public static final ImageIcon ICON_SORT = createImageIcon("32/download4.png");
+	public static final ImageIcon ICON_HELP = createImageIcon("32/32px-Help.png");
+	public static final ImageIcon ICON_EXIT = createImageIcon("32/exit.png");
+	public static final ImageIcon ICON_SETTINGS = createImageIcon("32/settings.png");
+	public static final ImageIcon ICON_BACKUP = createImageIcon("32/order.png");
+}
