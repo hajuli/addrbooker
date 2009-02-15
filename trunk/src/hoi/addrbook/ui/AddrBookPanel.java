771917690ca0bc3fedc6d63f5dcef7bf43332d75
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-02-15 12:04:32 +0000

    backup

--- trunk/src/hoi/addrbook/ui/AddrBookPanel.java
+++ trunk/src/hoi/addrbook/ui/AddrBookPanel.java
@@ -8,6 +8,8 @@ import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.FocusEvent;
@@ -40,152 +42,98 @@ public class AddrBookPanel extends JPanel {
 
 	private static final long serialVersionUID = -4479991916447167635L;
 
+	private static final Color BORDER_COLOR = new JLabel().getBackground();
+
+	private JButton tbarAddButton = new JButton("添加", ImageHelper.ICON_ADD);
+	private JButton tbarSaveButton = new JButton("保存", ImageHelper.ICON_SAVE);
+	private JButton tbarEditButton = new JButton("编辑", ImageHelper.ICON_EDIT);
+	private JButton tbarDeleteButton = new JButton("删除", ImageHelper.ICON_DELETE);
+	private JButton tbarSettingButton = new JButton("设置", ImageHelper.ICON_SETTING);
+	private JButton tbarBackupButton = new JButton("备份", ImageHelper.ICON_BACKUP);
+
 	private JTextField filterField = new JTextField();
 	private JList contactList = new JList();
-	private JTextField nameField = new JTextField();
-	private JTextField contactField = new JTextField();
-	private JTextField birthdayField = new JTextField();
-	private JTextField ageField = new JTextField();
-	
-	private JTextArea notesArea = new JTextArea();
-	private JTextField[] infoFields = new JTextField[13];
 
-	private JTextField addrPathField = new JTextField("abcS");
-	private JMenuItem syncMailItem = new JMenuItem("Sync mail");
-	private JMenuItem configMailItem = new JMenuItem("Config mail");
-	private JMenuItem usageItem = new JMenuItem("Usage");
-	private JMenuItem aboutItem = new JMenuItem("About");
+	private JTextField infoNameField = new JTextField();
+	private JTextField infoContactField = new JTextField();
+	private JTextField infoBirthdayField = new JTextField();
+	private JTextField infoAgeField = new JTextField();
+	private JTextField infoQQField = new JTextField();
+	private JTextField infoMSNField = new JTextField();
+	private JTextField infoMobileField = new JTextField();
+	private JTextField infoFetionField = new JTextField();
+	private JTextField infoEMailField = new JTextField();
+	private JTextField infoWebsiteField = new JTextField();
+	private JTextField infoHomeAddrField = new JTextField();
+	private JTextField infoCompanyNameField = new JTextField();
+	private JTextField infoCompanyAddrField = new JTextField();
+	private JTextArea infoNoteArea = new JTextArea();
 
-	class MyCellRenderer extends JLabel implements ListCellRenderer {
-		public MyCellRenderer() {
-			setOpaque(true);
+	private JPanel createInfoPanel(String[] names, JComponent[] comps) {
+		JPanel aPanel = new JPanel(new GridLayout(names.length, 1, 3, 3));
+		for (String name : names) {
+			JLabel l = new JLabel(" " + name + ":", JLabel.RIGHT);
+			l.setBorder(BorderFactory.createEtchedBorder());
+			aPanel.add(l); // 两端对齐
 		}
-
-		public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-			//	setBackground(isSelected ? Color.BLUE : Color.LIGHT_GRAY);
-			setBackground(isSelected ? Color.red : Color.white);
-			setForeground(isSelected ? Color.white : Color.black);
-
-			setHorizontalAlignment(SwingConstants.RIGHT);
-			setText(value.toString());
-			return this;
+		JPanel bPanel = new JPanel(new GridLayout(comps.length, 1, 3, 3));
+		for (JComponent comp : comps) {
+			comp.setBorder(BorderFactory.createEtchedBorder());
+			bPanel.add(comp);
 		}
+		JPanel cPanel = new JPanel(new BorderLayout(3, 3));
+		cPanel.add(aPanel, BorderLayout.WEST);
+		cPanel.add(bPanel, BorderLayout.CENTER);
+		return cPanel;
+	}
+
+	private JPanel createInfoPanel() {
+		JPanel aPanel = new JPanel(new GridLayout(1, 2, 3, 0));
+		aPanel.add(createInfoPanel(new String[] { "姓名", "生日", "QQ", "手机" },//
+				new JComponent[] { infoNameField, infoBirthdayField, infoQQField, infoMobileField }));
+		aPanel.add(createInfoPanel(new String[] { "联系", "年龄", "MSN", "飞信" },//
+				new JComponent[] { infoContactField, infoAgeField, infoMSNField, infoFetionField }));
+
+		JPanel bPanel = new JPanel(new BorderLayout(2, 2));
+		bPanel.add(aPanel, BorderLayout.NORTH);
+		bPanel.add(createInfoPanel(new String[] { "电子邮箱", "个人主页", "家庭住址", "公司名称", "公司地址" },//
+				new JComponent[] { infoEMailField, infoWebsiteField, infoHomeAddrField, infoCompanyNameField, infoCompanyAddrField })//
+				, BorderLayout.SOUTH);
+
+		JPanel cPanel = new JPanel(new BorderLayout(3, 3));
+		cPanel.add(bPanel, BorderLayout.NORTH);
+		infoNoteArea.setBorder(BorderFactory.createEtchedBorder());
+		cPanel.add(infoNoteArea, BorderLayout.CENTER);
+		cPanel.setBorder(BorderFactory.createEtchedBorder());
+		cPanel.setMinimumSize(new Dimension(320, -1));
+		return cPanel;
 	}
 
 	public AddrBookPanel() {
-		addrPathField.setEditable(false);
-		addrPathField.setBorder(BorderFactory.createEmptyBorder());
-		addrPathField.setCursor(new Cursor(Cursor.TEXT_CURSOR));
-
-		JPanel tPanel = new JPanel(new BorderLayout(0, 0));
-		filterField.setPreferredSize(new Dimension(-1, 25));
-		//	orderbyChoose.setPreferredSize(new Dimension(-1, 25));
-		//	orderbyChoose.setRenderer(new MyCellRenderer());
-		tPanel.add(filterField, BorderLayout.CENTER);
-		filterField.setBorder(BorderFactory.createEmptyBorder());
-		//	tPanel.add(orderbyChoose, BorderLayout.EAST);
 		filterField.setBorder(BorderFactory.createEtchedBorder());
+		//	filterField.setInsets(new Insets(2,2,2,2));
+		contactList.setBorder(BorderFactory.createEmptyBorder());
+		JScrollPane scroll = new JScrollPane(contactList);
+		scroll.setBorder(BorderFactory.createEtchedBorder());
+		JPanel wPanel = new JPanel(new BorderLayout(0, 2));
+		wPanel.add(filterField, BorderLayout.NORTH);
+		wPanel.add(scroll, BorderLayout.CENTER);
+		wPanel.setMinimumSize(new Dimension(150, -1));
 
-		notesArea.setPreferredSize(new Dimension(-1, 100));
-
-		JPanel wPanel = new JPanel(new BorderLayout(0, 0));
-		wPanel.add(tPanel, BorderLayout.NORTH);
-
-		JScrollPane a = new JScrollPane(contactList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, //
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		a.setBorder(BorderFactory.createEmptyBorder());
-		a.setPreferredSize(new Dimension(-1, 100));
-		//JSplitPane wsPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, a, b);
-
-		contactList.setBackground(Color.WHITE);
-		contactList.setBorder(BorderFactory.createEtchedBorder());
-
-		wPanel.add(a, BorderLayout.CENTER);
-		//wPanel.setBorder(BorderFactory.createEtchedBorder());
-		wPanel.setPreferredSize(new Dimension(150, -1));
-
-		InfoPanel infoPanel = new InfoPanel();
-		infoPanel.setBorder(BorderFactory.createEmptyBorder(10, 5, 10, 10));
-
-		JButton sortButton = new JButton("排序", ImageHelper.ICON_SORT);
-		JButton addButton = new JButton("添加", ImageHelper.ICON_ADD);
-		JButton saveButton = new JButton("保存", ImageHelper.ICON_SAVE);
-		JButton editButton = new JButton("编辑", ImageHelper.ICON_EDIT);
-		JButton deleteButton = new JButton("删除", ImageHelper.ICON_DELETE);
-		JButton settingsButton = new JButton("设置", ImageHelper.ICON_SETTING);
-		JButton backupButton = new JButton("备份", ImageHelper.ICON_BACKUP);
-		JButton helpButton = new JButton("帮助", ImageHelper.ICON_HELP);
-		JButton exitButton = new JButton("退出", ImageHelper.ICON_EXIT);
-
-		JToolBar toolbar = new JToolBar();
+		JToolBar toolbar = new JToolBar("工具条");
 		toolbar.setFloatable(false);
 		toolbar.setLayout(new GridLayout(1, 6));
-		//	toolbar.add(sortButton);
-		toolbar.add(addButton);
-		toolbar.add(saveButton);
-		toolbar.add(editButton);
-		toolbar.add(deleteButton);
-		toolbar.add(settingsButton);
-		toolbar.add(backupButton);
-		//toolbar.add(printButton);
-		//	toolbar.add(helpButton);
-		//	toolbar.add(exitButton);
+		toolbar.add(tbarAddButton);
+		toolbar.add(tbarSaveButton);
+		toolbar.add(tbarEditButton);
+		toolbar.add(tbarDeleteButton);
+		toolbar.add(tbarSettingButton);
+		toolbar.add(tbarBackupButton);
 
 		setLayout(new BorderLayout());
 		add(toolbar, BorderLayout.NORTH);
-		JSplitPane wtPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, wPanel, infoPanel);
-		Border border = BorderFactory.createEmptyBorder(5, 5, 5, 5);
-		wtPane.setBorder(border);
+		JSplitPane wtPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, wPanel, createInfoPanel());
+		wtPane.setBorder(BorderFactory.createLineBorder(BORDER_COLOR, 5));
 		add(wtPane, BorderLayout.CENTER);
-	}
-}
-
-class InfoPanel extends JPanel {
-	private static final long serialVersionUID = -2588763425591993699L;
-	private String[][] info = { { "姓名", "联系" },//
-			{ "生日", "年龄" }, //
-			{ "QQ", "MSN" },//ＭＳＮ
-			{ "手机", "飞信", },// 
-			{ "电子邮箱", },//
-			{ "个人主页", },//
-			{ "公司名称", }, //
-			{ "公司地址" }, //
-	};
-
-	public InfoPanel() {
-		JPanel cp = new JPanel(new GridLayout(info.length, 1, 5, 5));
-		for (int i = 0; i < info.length; i++) {
-			JPanel tp = new JPanel(new GridLayout(1, info[i].length, 2, 3));
-			for (int j = 0; j < info[i].length; j++)
-				tp.add(new AddrBookTextField(info[i][j]));
-			cp.add(tp);
-		}
-
-		setLayout(new BorderLayout(5, 5));
-		JTextField k = new JTextField();
-		k.setEditable(false);
-		//add(k, BorderLayout.NORTH);
-		add(cp, BorderLayout.NORTH);
-
-		final JTextArea kd = new JTextArea();
-		JScrollPane b = new JScrollPane(kd, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, //
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		kd.setBackground(new JLabel().getBackground());
-		kd.addFocusListener(new FocusListener() {
-
-			@Override
-			public void focusGained(FocusEvent e) {
-				kd.setBackground(Color.WHITE);
-				kd.setBorder(BorderFactory.createEtchedBorder());
-			}
-
-			@Override
-			public void focusLost(FocusEvent e) {
-				kd.setBackground(new JLabel().getBackground());
-				kd.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-			}
-
-		});
-		add(kd, BorderLayout.CENTER);
 	}
 }
