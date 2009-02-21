a90ce641829d8fc634fa1702a7e95f80654f604a
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-02-21 03:11:29 +0000

    AddrBookBackupDialog

--- src/hoi/addrbook/ui/AddrBookBackupDialog.java
+++ src/hoi/addrbook/ui/AddrBookBackupDialog.java
@@ -1,33 +1,23 @@
 package hoi.addrbook.ui;
 
-import hoi.addrbook.icons.ImageHelper;
-
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Frame;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
 
 public class AddrBookBackupDialog extends JDialog {
 
-	private static final Color BORDER_COLOR = new JLabel().getBackground();
+	private static final long serialVersionUID = -3557688446186478068L;
 
-	private String infoAddrBooker = "<html>草根cǎo gēn通讯录<br>AddrBooker<br>http://code.google.com/p/addrbooker/<br>" + //
-			"合群之草，才有力量<br>野火烧不尽，春风吹又生";
+	private static final int BLANK_SIZE = 20;
+	private static final int GAP_SIZE = 5;
 
 	private JButton backupButton = new JButton("备份数据");
 	private JButton restoreButton = new JButton("恢复数据");
@@ -35,74 +25,37 @@ public class AddrBookBackupDialog extends JDialog {
 	private JButton helpButton = new JButton("帮助");
 	private JButton closeButton = new JButton("关闭");
 
-	public AddrBookBackupDialog() {
-		JPanel aPanel = new JPanel();
-		final JButton b = new JButton(infoAddrBooker, ImageHelper.ICON_LOGO_KUD);
-		aPanel.add(b);
-		aPanel.setBorder(BorderFactory.createEtchedBorder());
-		b.addMouseListener(new MouseListener() {
+	public AddrBookBackupDialog(Frame owner) {
+		super(owner);
 
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
+		JPanel aPanel = new JPanel(new GridLayout(1, 2, GAP_SIZE * 2, GAP_SIZE));
+		aPanel.add(backupButton);
+		aPanel.add(restoreButton);
+		aPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEtchedBorder(), //
+				new EmptyBorder(new Insets(BLANK_SIZE, BLANK_SIZE * 2, BLANK_SIZE, BLANK_SIZE * 2))));
 
-			}
+		JPanel bPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, GAP_SIZE * 2, GAP_SIZE));
+		bPanel.add(helpButton);
+		bPanel.add(closeButton);
+		// bPanel.setBorder(BorderFactory.createEtchedBorder());
 
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				b.setIcon(ImageHelper.ICON_LOGO_KUD);
-			}
+		JPanel cPanel = new JPanel(new BorderLayout(BLANK_SIZE, BLANK_SIZE));
+		cPanel.add(aPanel, BorderLayout.CENTER);
+		cPanel.add(bPanel, BorderLayout.SOUTH);
+		cPanel.setBorder(new EmptyBorder(new Insets(BLANK_SIZE, BLANK_SIZE, BLANK_SIZE, BLANK_SIZE)));
 
-			@Override
-			public void mouseExited(MouseEvent e) {
-				b.setIcon(ImageHelper.ICON_LOGO_KUD);
-			}
+		setLayout(new BorderLayout(0, 0));
+		add(cPanel, BorderLayout.CENTER);
 
-			@Override
-			public void mousePressed(MouseEvent e) {
-				b.setIcon(ImageHelper.ICON_LOGO_DATA);
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				b.setIcon(ImageHelper.ICON_LOGO_KUD);
-			}
-
-		});
-
-		JPanel bPanel = new JPanel(new GridLayout(1, 2));
-		bPanel.add(backupButton);
-		bPanel.add(restoreButton);
-		bPanel.setBorder(BorderFactory.createEtchedBorder());
-
-		JPanel cPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		cPanel.add(helpButton);
-		cPanel.add(closeButton);
-
-		JPanel mPanel = new JPanel(new BorderLayout());
-		mPanel.add(aPanel, BorderLayout.NORTH);
-		mPanel.add(bPanel, BorderLayout.CENTER);
-		mPanel.add(cPanel, BorderLayout.SOUTH);
-		mPanel.setBorder(BorderFactory.createLineBorder(BORDER_COLOR, 5));
-		this.setContentPane(mPanel);
-	}
-
-	private static void setLookAndFeel() {
-		try { // Default, Green, Lemmon, Red
-			com.jtattoo.plaf.acryl.AcrylLookAndFeel.setTheme("Default", "??", "Addrbooker");
-			UIManager.setLookAndFeel(new com.jtattoo.plaf.acryl.AcrylLookAndFeel());
-		} catch (UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
+		if (owner != null)
+			setIconImage(owner.getIconImage());
+		setTitle("备份/恢复 数据");
+		pack();
+		setResizable(false);
+		setLocationRelativeTo(owner);
 	}
 
 	public static void main(String[] args) {
-		setLookAndFeel();
-		AddrBookBackupDialog d = new AddrBookBackupDialog();
-		d.pack();
-		d.setTitle("abc");
-		d.setIconImage(ImageHelper.ICON_LOGO.getImage());
-		d.setLocationRelativeTo(null);
-		d.setVisible(true);
+		new AddrBookBackupDialog(null).setVisible(true);
 	}
 }
