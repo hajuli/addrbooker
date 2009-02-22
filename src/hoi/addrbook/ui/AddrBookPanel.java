55f2f81f41c5ead0e939891553da153c12d4aeaa
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-02-22 09:04:01 +0000

    backup

--- src/hoi/addrbook/ui/AddrBookPanel.java
+++ src/hoi/addrbook/ui/AddrBookPanel.java
@@ -9,6 +9,8 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -38,8 +40,30 @@ public class AddrBookPanel extends JPanel {
 	private JButton tbarSaveButton = new JButton("保存", ImageHelper.ICON_SAVE);
 	private JButton tbarEditButton = new JButton("编辑", ImageHelper.ICON_EDIT);
 	private JButton tbarDeleteButton = new JButton("删除", ImageHelper.ICON_DELETE);
-	private JButton tbarSettingButton = new JButton("设置", ImageHelper.ICON_SETTING);
-	private JButton tbarBackupButton = new JButton("备份", ImageHelper.ICON_BACKUP);
+	private JButton tbarSettingButton = new JButton("设置", ImageHelper.ICON_SETTING) {
+		{
+			this.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					new AddrBookSettingDialog(frame).setVisible(true);
+				}
+
+			});
+		}
+	};
+	private JButton tbarBackupButton = new JButton("备份", ImageHelper.ICON_BACKUP) {
+		{
+			this.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					new AddrBookBackupDialog(frame).setVisible(true);
+				}
+
+			});
+		}
+	};
 
 	private JTextField filterField = new JTextField() {
 		public void paint(Graphics g) {
@@ -58,23 +82,24 @@ public class AddrBookPanel extends JPanel {
 			}
 		}
 	};
-	private JList contactList = new JList(new String[] {"杨全海",
-			"杨全海","姐姐","妈妈"});
+	private JList contactList = new JList(new String[] {
+			"杨全海", "杨全海", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐", "叔叔#妈妈", "喷粪", "杨东，哥哥", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐",
+			"妈妈", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐", "妈妈", "姐姐", "妈妈" });
 
-	private JTextField infoNameField = new JTextField();
+	private JTextField infoNameField = new JTextField("杨全海");
 	private ContactField infoContactField = new ContactField();
 	private BirthdayField infoBirthdayField = new BirthdayField();
-	private JTextField infoAgeField = new JTextField();
-	private JTextField infoQQField = new JTextField();
-	private JTextField infoMSNField = new JTextField();
-	private JTextField infoMobileField = new JTextField();
-	private JTextField infoFetionField = new JTextField();
-	private JTextField infoEMailField = new JTextField();
-	private JTextField infoWebsiteField = new JTextField();
-	private JTextField infoHomeAddrField = new JTextField();
-	private JTextField infoCompanyNameField = new JTextField();
-	private JTextField infoCompanyAddrField = new JTextField();
-	private JTextArea infoNoteArea = new JTextArea();
+	private JTextField infoAgeField = new JTextField("23");
+	private JTextField infoQQField = new JTextField("332910789");
+	private JTextField infoMSNField = new JTextField("haihoing@live.cn");
+	private JTextField infoMobileField = new JTextField("13825622953");
+	private JTextField infoFetionField = new JTextField("293401164");
+	private JTextField infoEMailField = new JTextField("haihoing@gmail.com");
+	private JTextField infoWebsiteField = new JTextField("http://332910789.qzone.qq.com");
+	private JTextField infoHomeAddrField = new JTextField("珠海吉大海湾10栋8A");
+	private JTextField infoCompanyNameField = new JTextField("珠海金山软件; 珠海吉大石花西路601号");
+	private JTextField infoCompanyAddrField = new JTextField("珠海吉大石花西路601号");
+	private JTextArea infoNoteArea = new JTextArea("MSG");
 
 	private JPanel createInfoPanel(String[] names, JComponent[] comps) {
 		JPanel aPanel = new JPanel(new GridLayout(names.length, 1, GAP_SIZE, GAP_SIZE));
@@ -105,16 +130,16 @@ public class AddrBookPanel extends JPanel {
 				new JComponent[] {
 						infoNameField, infoBirthdayField, infoQQField, infoMobileField }));
 		aPanel.add(createInfoPanel(new String[] {
-				"联系", "年龄", "MSN", "飞信" }, //
+				"计时", "年龄", "MSN", "飞信" }, //
 				new JComponent[] {
 						infoContactField, infoAgeField, infoMSNField, infoFetionField }));
 
 		JPanel bPanel = new JPanel(new BorderLayout(GAP_SIZE, GAP_SIZE));
 		bPanel.add(aPanel, BorderLayout.NORTH);
 		bPanel.add(createInfoPanel(new String[] {
-				"电子邮箱", "个人主页", "家庭住址", "公司名称", "公司地址" }, //
+				"电子邮箱", "个人主页", "家庭住址", "公司信息" }, //
 				new JComponent[] {
-						infoEMailField, infoWebsiteField, infoHomeAddrField, infoCompanyNameField, infoCompanyAddrField }), //
+						infoEMailField, infoWebsiteField, infoHomeAddrField, infoCompanyNameField }), //
 				BorderLayout.CENTER);
 
 		JPanel cPanel = new JPanel(new BorderLayout(GAP_SIZE, GAP_SIZE));
@@ -122,11 +147,14 @@ public class AddrBookPanel extends JPanel {
 		infoNoteArea.setBorder(COMMON_BORDER);
 		cPanel.add(infoNoteArea, BorderLayout.CENTER);
 		cPanel.setBorder(BorderFactory.createEtchedBorder());
-		cPanel.setMinimumSize(new Dimension(365, -1));
+		cPanel.setMinimumSize(new Dimension(370, -1));
 		return cPanel;
 	}
 
-	public AddrBookPanel() {
+	AddrBookFrame frame;
+
+	public AddrBookPanel(AddrBookFrame frame) {
+		this.frame = frame;
 		filterField.setBorder(COMMON_BORDER);
 		filterField.getInsets();
 		contactList.setBorder(BorderFactory.createEmptyBorder());
@@ -135,7 +163,7 @@ public class AddrBookPanel extends JPanel {
 		JPanel wPanel = new JPanel(new BorderLayout(GAP_SIZE, GAP_SIZE));
 		wPanel.add(filterField, BorderLayout.NORTH);
 		wPanel.add(scroll, BorderLayout.CENTER);
-		wPanel.setMinimumSize(new Dimension(120, -1));
+		wPanel.setMinimumSize(new Dimension(125, -1));
 		wPanel.setBorder(BorderFactory.createEtchedBorder());
 
 		JToolBar toolbar = new JToolBar("工具条");
