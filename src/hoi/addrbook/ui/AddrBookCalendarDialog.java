8fdcf4705062dea2c4960ff8158bf50a1a2f648b
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-02-22 02:13:51 +0000

    backup

--- src/hoi/addrbook/ui/AddrBookCalendarDialog.java
+++ src/hoi/addrbook/ui/AddrBookCalendarDialog.java
@@ -107,7 +107,8 @@ public class AddrBookCalendarDialog extends JFrame implements ActionListener {
 
 		panel.setLayout(new GridLayout(1, 7));
 
-		String columnNames[] = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+		String columnNames[] = {
+				"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
 		JLabel label = null;
 		for (int i = 0; i < 7; i++) {
 			label = new JLabel(columnNames[i], JLabel.CENTER);
@@ -454,13 +455,25 @@ public class AddrBookCalendarDialog extends JFrame implements ActionListener {
 
 	class List_MouseListener implements MouseListener {//鼠标移入时显示的信息
 		JLabel labe = null;
-		String weeks[] = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+		String weeks[] = {
+				"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
 
 		public String constellation(Calendar cal) {
 
-			String[][] con = { { "水瓶座", "0122", "0221" }, { "双鱼座", "0222", "0321" }, { "白羊座", "0322", "0420" }, { "金牛座", "0421", "0521" },
-					{ "双子座", "0522", "0621" }, { "巨蟹座", "0621", "0721" }, { "狮子座", "0722", "0821" }, { "处女座", "0822", "0921" }, { "天秤座", "0922", "1021" },
-					{ "天蝎座", "1022", "1121" }, { "射手座", "1122", "1221" }, { "摩羯座", "1222", "0121" } };
+			String[][] con = {
+					{
+							"水瓶座", "0122", "0221" }, {
+							"双鱼座", "0222", "0321" }, {
+							"白羊座", "0322", "0420" }, {
+							"金牛座", "0421", "0521" }, {
+							"双子座", "0522", "0621" }, {
+							"巨蟹座", "0621", "0721" }, {
+							"狮子座", "0722", "0821" }, {
+							"处女座", "0822", "0921" }, {
+							"天秤座", "0922", "1021" }, {
+							"天蝎座", "1022", "1121" }, {
+							"射手座", "1122", "1221" }, {
+							"摩羯座", "1222", "0121" } };
 			int month = cal.get(Calendar.MONTH) + 1;
 			int today = cal.get(Calendar.DAY_OF_MONTH);
 			String month_str = month < 10 ? "0" + month : "" + month;
@@ -543,22 +556,25 @@ class Lunar {
 	private int month;
 	private int day;
 	private boolean leap;
-	final static String chineseNumber[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二" };
-	final static String Big_Or_Small[] = { "大", "小", "大", "小", "大", "小", "大", "大", "小", "大", "小", "大" };
-	private String[] LunarHolDayName = { "小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏", "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑", "白露", "秋分", "寒露",
-			"霜降", "立冬", "小雪", "大雪", "冬至" };
+	final static String chineseNumber[] = {
+			"一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二" };
+	final static String Big_Or_Small[] = {
+			"大", "小", "大", "小", "大", "小", "大", "大", "小", "大", "小", "大" };
+	private String[] LunarHolDayName = {
+			"小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏", "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑", "白露", "秋分", "寒露", "霜降", "立冬", "小雪", "大雪", "冬至" };
 
 	static SimpleDateFormat chineseDateFormat = new SimpleDateFormat(" yyyy年MM月dd日 ");
-	final static long[] lunarInfo = new long[] { 0x04bd8, 0x04ae0, 0x0a570, 0x054d5, 0x0d260, 0x0d950, 0x16554, 0x056a0, 0x09ad0, 0x055d2, 0x04ae0, 0x0a5b6,
-			0x0a4d0, 0x0d250, 0x1d255, 0x0b540, 0x0d6a0, 0x0ada2, 0x095b0, 0x14977, 0x04970, 0x0a4b0, 0x0b4b5, 0x06a50, 0x06d40, 0x1ab54, 0x02b60, 0x09570,
-			0x052f2, 0x04970, 0x06566, 0x0d4a0, 0x0ea50, 0x06e95, 0x05ad0, 0x02b60, 0x186e3, 0x092e0, 0x1c8d7, 0x0c950, 0x0d4a0, 0x1d8a6, 0x0b550, 0x056a0,
-			0x1a5b4, 0x025d0, 0x092d0, 0x0d2b2, 0x0a950, 0x0b557, 0x06ca0, 0x0b550, 0x15355, 0x04da0, 0x0a5b0, 0x14573, 0x052b0, 0x0a9a8, 0x0e950, 0x06aa0,
-			0x0aea6, 0x0ab50, 0x04b60, 0x0aae4, 0x0a570, 0x05260, 0x0f263, 0x0d950, 0x05b57, 0x056a0, 0x096d0, 0x04dd5, 0x04ad0, 0x0a4d0, 0x0d4d4, 0x0d250,
-			0x0d558, 0x0b540, 0x0b6a0, 0x195a6, 0x095b0, 0x049b0, 0x0a974, 0x0a4b0, 0x0b27a, 0x06a50, 0x06d40, 0x0af46, 0x0ab60, 0x09570, 0x04af5, 0x04970,
-			0x064b0, 0x074a3, 0x0ea50, 0x06b58, 0x055c0, 0x0ab60, 0x096d5, 0x092e0, 0x0c960, 0x0d954, 0x0d4a0, 0x0da50, 0x07552, 0x056a0, 0x0abb7, 0x025d0,
-			0x092d0, 0x0cab5, 0x0a950, 0x0b4a0, 0x0baa4, 0x0ad50, 0x055d9, 0x04ba0, 0x0a5b0, 0x15176, 0x052b0, 0x0a930, 0x07954, 0x06aa0, 0x0ad50, 0x05b52,
-			0x04b60, 0x0a6e6, 0x0a4e0, 0x0d260, 0x0ea65, 0x0d530, 0x05aa0, 0x076a3, 0x096d0, 0x04bd7, 0x04ad0, 0x0a4d0, 0x1d0b6, 0x0d250, 0x0d520, 0x0dd45,
-			0x0b5a0, 0x056d0, 0x055b2, 0x049b0, 0x0a577, 0x0a4b0, 0x0aa50, 0x1b255, 0x06d20, 0x0ada0, 0x14b63 };
+	final static long[] lunarInfo = new long[] {
+			0x04bd8, 0x04ae0, 0x0a570, 0x054d5, 0x0d260, 0x0d950, 0x16554, 0x056a0, 0x09ad0, 0x055d2, 0x04ae0, 0x0a5b6, 0x0a4d0, 0x0d250, 0x1d255, 0x0b540,
+			0x0d6a0, 0x0ada2, 0x095b0, 0x14977, 0x04970, 0x0a4b0, 0x0b4b5, 0x06a50, 0x06d40, 0x1ab54, 0x02b60, 0x09570, 0x052f2, 0x04970, 0x06566, 0x0d4a0,
+			0x0ea50, 0x06e95, 0x05ad0, 0x02b60, 0x186e3, 0x092e0, 0x1c8d7, 0x0c950, 0x0d4a0, 0x1d8a6, 0x0b550, 0x056a0, 0x1a5b4, 0x025d0, 0x092d0, 0x0d2b2,
+			0x0a950, 0x0b557, 0x06ca0, 0x0b550, 0x15355, 0x04da0, 0x0a5b0, 0x14573, 0x052b0, 0x0a9a8, 0x0e950, 0x06aa0, 0x0aea6, 0x0ab50, 0x04b60, 0x0aae4,
+			0x0a570, 0x05260, 0x0f263, 0x0d950, 0x05b57, 0x056a0, 0x096d0, 0x04dd5, 0x04ad0, 0x0a4d0, 0x0d4d4, 0x0d250, 0x0d558, 0x0b540, 0x0b6a0, 0x195a6,
+			0x095b0, 0x049b0, 0x0a974, 0x0a4b0, 0x0b27a, 0x06a50, 0x06d40, 0x0af46, 0x0ab60, 0x09570, 0x04af5, 0x04970, 0x064b0, 0x074a3, 0x0ea50, 0x06b58,
+			0x055c0, 0x0ab60, 0x096d5, 0x092e0, 0x0c960, 0x0d954, 0x0d4a0, 0x0da50, 0x07552, 0x056a0, 0x0abb7, 0x025d0, 0x092d0, 0x0cab5, 0x0a950, 0x0b4a0,
+			0x0baa4, 0x0ad50, 0x055d9, 0x04ba0, 0x0a5b0, 0x15176, 0x052b0, 0x0a930, 0x07954, 0x06aa0, 0x0ad50, 0x05b52, 0x04b60, 0x0a6e6, 0x0a4e0, 0x0d260,
+			0x0ea65, 0x0d530, 0x05aa0, 0x076a3, 0x096d0, 0x04bd7, 0x04ad0, 0x0a4d0, 0x1d0b6, 0x0d250, 0x0d520, 0x0dd45, 0x0b5a0, 0x056d0, 0x055b2, 0x049b0,
+			0x0a577, 0x0a4b0, 0x0aa50, 0x1b255, 0x06d20, 0x0ada0, 0x14b63 };
 
 	// ====== 传回农历 y年的总天数
 	final private static int yearDays(int y) {
@@ -596,14 +612,17 @@ class Lunar {
 
 	// ====== 传回农历 y年的生肖
 	final public String animalsYear() {
-		final String[] Animals = new String[] { "鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪" };
+		final String[] Animals = new String[] {
+				"鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪" };
 		return Animals[(year - 4) % 12];
 	}
 
 	// ====== 传入 月日的offset 传回干支, 0=甲子
 	final private static String cyclicalm(int num) {
-		final String[] Gan = new String[] { "甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸" };
-		final String[] Zhi = new String[] { "子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥" };
+		final String[] Gan = new String[] {
+				"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸" };
+		final String[] Zhi = new String[] {
+				"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥" };
 		return (Gan[num % 10] + Zhi[num % 12]);
 	}
 
@@ -699,7 +718,8 @@ class Lunar {
 	}
 
 	public static String getChinaDayString(int day) {
-		String chineseTen[] = { "初", "十", "廿", "卅" };
+		String chineseTen[] = {
+				"初", "十", "廿", "卅" };
 		int n = day % 10 == 0 ? 9 : day % 10 - 1;
 		if (day > 30)
 			return "";
