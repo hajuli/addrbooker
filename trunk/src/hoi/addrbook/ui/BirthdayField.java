476e956e4478ff9500fb76d673f1d389fc3efc35
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-02-22 03:34:51 +0000

    backup

---
+++ trunk/src/hoi/addrbook/ui/BirthdayField.java
@@ -0,0 +1,33 @@
+package hoi.addrbook.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+public class BirthdayField extends JPanel {
+	
+	private static final long serialVersionUID = -2283281574591552446L;
+	private static final Border COMMON_BORDER = BorderFactory.createCompoundBorder( //
+			BorderFactory.createEtchedBorder(), //
+			new EmptyBorder(new Insets(1, 1, 1, 1)));
+	
+	private JTextField solarField = new JTextField();
+	private JTextField lunarField = new JTextField();
+
+	public BirthdayField() {
+		solarField.setBorder(COMMON_BORDER);
+		lunarField.setBorder(COMMON_BORDER);
+
+		setLayout(new GridLayout(1, 2, -1, -1));
+		add(solarField);
+		add(lunarField);
+	}
+}
