607805710b2be0ef3e1fa979e4d8ecac061e2c4d
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-02-25 13:49:50 +0000

    backup

--- src/hoi/addrbook/ui/AddrBookSettingDialog.java
+++ src/hoi/addrbook/ui/AddrBookSettingDialog.java
@@ -9,9 +9,7 @@ import java.awt.Insets;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
 
 public class AddrBookSettingDialog extends JDialog {
@@ -23,7 +21,6 @@ public class AddrBookSettingDialog extends JDialog {
 
 	private JButton addPasswordButton = new JButton("设置密码");
 	private JButton changePasswordButton = new JButton("更改密码");
-	private JButton deletePasswordButton = new JButton("删除密码");
 
 	private JButton donateButton = new JButton("捐赠");
 	private JButton closeButton = new JButton("关闭");
@@ -31,26 +28,24 @@ public class AddrBookSettingDialog extends JDialog {
 	public AddrBookSettingDialog(Frame owner) {
 		super(owner);
 
-		JPanel bPanel = new JPanel(new GridLayout(3, 1, GAP_SIZE * 2, GAP_SIZE));
-		bPanel.add(addPasswordButton);
-		bPanel.add(changePasswordButton);
-		bPanel.add(deletePasswordButton);
-		bPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder("密码保护"), //
-				new EmptyBorder(new Insets(BLANK_SIZE / 2, BLANK_SIZE, BLANK_SIZE / 2, BLANK_SIZE))));
+		JPanel aPanel = new JPanel(new GridLayout(1, 2, GAP_SIZE * 2, GAP_SIZE));
+		aPanel.add(addPasswordButton);
+		aPanel.add(changePasswordButton);
+		aPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEtchedBorder(), //
+				new EmptyBorder(new Insets(BLANK_SIZE, BLANK_SIZE, BLANK_SIZE, BLANK_SIZE))));
 
-		JPanel cPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, GAP_SIZE * 2, GAP_SIZE));
-		cPanel.add(new JLabel("            "));
-		cPanel.add(donateButton);
-		cPanel.add(closeButton);
-		// cPanel.setBorder(BorderFactory.createEtchedBorder());
+		JPanel bPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, GAP_SIZE * 2, GAP_SIZE));
+		bPanel.add(donateButton);
+		bPanel.add(closeButton);
+		// bPanel.setBorder(BorderFactory.createEtchedBorder());
 
-		JPanel mPanel = new JPanel(new BorderLayout(BLANK_SIZE, BLANK_SIZE / 2));
-		mPanel.add(bPanel, BorderLayout.CENTER);
-		mPanel.add(cPanel, BorderLayout.SOUTH);
-		mPanel.setBorder(new EmptyBorder(new Insets(BLANK_SIZE, BLANK_SIZE, BLANK_SIZE, BLANK_SIZE)));
+		JPanel cPanel = new JPanel(new BorderLayout(BLANK_SIZE, BLANK_SIZE));
+		cPanel.add(aPanel, BorderLayout.CENTER);
+		cPanel.add(bPanel, BorderLayout.SOUTH);
+		cPanel.setBorder(new EmptyBorder(new Insets(BLANK_SIZE, BLANK_SIZE, BLANK_SIZE, BLANK_SIZE)));
 
 		setLayout(new BorderLayout(0, 0));
-		add(mPanel, BorderLayout.CENTER);
+		add(cPanel, BorderLayout.CENTER);
 
 		if (owner != null)
 			setIconImage(owner.getIconImage());
