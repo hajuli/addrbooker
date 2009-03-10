1d3d64838b423dc2e1cc99d41c377c5cf4c36848
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-03-10 15:15:18 +0000

    backup

--- src/hoi/addrbook/data/AddrBookProps.java
+++ src/hoi/addrbook/data/AddrBookProps.java
@@ -2,19 +2,21 @@ package hoi.addrbook.data;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 import org.apache.commons.io.FileUtils;
-import org.ho.yaml.Yaml;
 
 public class AddrBookProps extends java.util.LinkedHashMap<String, ContactProps> {
 
 	private static final long serialVersionUID = 2738957830618139780L;
 	private static final String USER_HOME_DIR = System.getProperty("user.home");
 	private static final String ADDRBOOKER_DIR_NAME = USER_HOME_DIR + File.separator + "AddrBooker";
-	private static final String ADDRBOOKER_FILE_PATH = ADDRBOOKER_DIR_NAME + File.separator + "AddrBooker.yml";
+	private static final String ADDRBOOKER_FILE_PATH = ADDRBOOKER_DIR_NAME + File.separator + "AddrBooker.abk";
 	static {
 		new File(ADDRBOOKER_DIR_NAME).mkdirs();
 		File file = new File(ADDRBOOKER_FILE_PATH);
@@ -29,21 +31,59 @@ public class AddrBookProps extends java.util.LinkedHashMap<String, ContactProps>
 				}
 			}
 		} else {
-			try {
-				Yaml.dump(new AddrBookProps(), new File(ADDRBOOKER_FILE_PATH));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+			save(new AddrBookProps(), ADDRBOOKER_FILE_PATH);
 		}
 	}
+	private static final String MARK = ": ";
 
 	private static String getDateString() {
 		return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
 	}
 
+	private static String quote(String str) {
+		if (str == null)
+			return "";
+		return str.replace("\n", "\\n").replace("\t", "\\t");
+	}
+
+	private static String unquote(String str) {
+		if (str == null)
+			return str;
+		return str.replace("\\n", "\n").replace("\\t", "\t");
+	}
+
 	public static AddrBookProps load(String path) {
 		try {
-			return (AddrBookProps) Yaml.load(new File(path));
+			String version = null;
+			AddrBookProps addrbook = new AddrBookProps();
+			ContactProps contact = new ContactProps();
+
+			Scanner cin = new Scanner(new File(path));
+			while (cin.hasNextLine()) {
+				String line = cin.nextLine();
+				if (line.startsWith("#")) {
+					version = line;
+				} else if (line.startsWith("+")) {
+					if (contact.containsKey(ContactPropsEnum.NAME.getKeyName())) {
+						String name = contact.getProperty(ContactPropsEnum.NAME);
+						addrbook.put(name, contact);
+						contact = new ContactProps();
+					} else {
+						//
+					}
+				} else if (line.indexOf(MARK) != -1) {
+					String[] o = line.split(MARK, 2);
+					if (o.length == 2) {
+						String key = o[0].trim();
+						String value = o[1];
+
+						contact.setProperty(key, unquote(value));
+					}
+				} else {
+
+				}
+			}
+			return addrbook;
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return new AddrBookProps();
@@ -56,8 +96,19 @@ public class AddrBookProps extends java.util.LinkedHashMap<String, ContactProps>
 
 	public static void save(AddrBookProps props, String path) {
 		try {
-			Yaml.dump(props, new File(path));
+			FileOutputStream fos = new FileOutputStream(path);
+			fos.write("#0.1\r\n".getBytes());
+			for (String s : props.keySet()) {
+				ContactProps contact = props.get(s);
+				for (Object ss : contact.keySet().toArray())
+					fos.write(String.format("%s%s%s\r\n", ss.toString(), MARK, quote(contact.getProperty( //
+							ss.toString()))).getBytes());
+				fos.write("+\r\n".getBytes());
+			}
+			fos.close();
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
--- src/hoi/addrbook/data/ContactProps.java
+++ src/hoi/addrbook/data/ContactProps.java
@@ -4,15 +4,36 @@ import java.util.Properties;
 
 public class ContactProps extends Properties {
 
+	public static final String NAME = ("姓名"); //
+	public static final String CLASSIFY = ("分组"); //
+	public static final String BIRTHDAY_LUNAR = ("生日_农历"); //
+	public static final String BIRTHDAY_SOLAR = ("生日_阳历"); //
+	public static final String TIMER = ("计时"); //
+	public static final String QQ = ("QQ"); //
+	public static final String MSN = ("MSN"); //
+	public static final String MOBILE = ("手机"); //
+	public static final String FETION = "飞信"; //
+	public static final String EMAIL = ("电子邮箱"); //
+	public static final String WEBSITE = ("个人主页"); //
+	public static final String HOMEADDR = ("家庭住址"); //
+	public static final String COMPANY = ("公司信息"); //
+	public static final String NOTES = ("附注"); //
+	public static final String AGE = ("年龄");
+
 	private static final long serialVersionUID = 2452503714339288830L;
+
+	public ContactProps() {
+		for (ContactPropsEnum key : ContactPropsEnum.values())
+			setProperty(key, "");
+	}
 
 	public Object setProperty(ContactPropsEnum contactKey, String value) {
 		return super.setProperty(contactKey.getKeyName(), value);
 	}
 
-	public Object setProperty(String key, String value) {
-		throw new IllegalArgumentException("This func not permitted");
-	}
+	//	public Object setProperty(String key, String value) {
+	//		throw new IllegalArgumentException("This func not permitted");
+	//	}
 
 	public String getProperty(ContactPropsEnum contactKey) {
 		return super.getProperty(contactKey.getKeyName());
@@ -21,12 +42,12 @@ public class ContactProps extends Properties {
 	public String getProperty(ContactPropsEnum contactKey, String defaultValue) {
 		return super.getProperty(contactKey.getKeyName(), defaultValue);
 	}
-
-	public String getProperty(String key) {
-		throw new IllegalArgumentException("This func not permitted");
-	}
-
-	public String getProperty(String key, String defaultValue) {
-		throw new IllegalArgumentException("This func not permitted");
-	}
+	//
+	//	public String getProperty(String key) {
+	//		throw new IllegalArgumentException("This func not permitted");
+	//	}
+	//
+	//	public String getProperty(String key, String defaultValue) {
+	//		throw new IllegalArgumentException("This func not permitted");
+	//	}
 }
