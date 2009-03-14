0859b776755d128ef2031ef1f73fc0c407ce5690
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-03-14 08:23:40 +0000

    ImageHelper

--- trunk/src/hoi/addrbook/icon/ImageHelper.java
+++ trunk/src/hoi/addrbook/icon/ImageHelper.java
@@ -9,43 +9,42 @@ import javax.swing.ImageIcon;
 
 public class ImageHelper {
 
-	private ImageHelper() {
-	}
+    private ImageHelper() {
+    }
 
-	/** Returns an ImageIcon, or null if the path was invalid. */
-	public static ImageIcon createImageIcon(String path) {
-		URL imgURL = ImageHelper.class.getResource(path);
-		if (imgURL != null) {
-			return new ImageIcon(imgURL);
-		} else {
-			System.err.println("Couldn't find file: " + path);
-			return null;
-		}
-	}
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    public static ImageIcon createImageIcon(String path) {
+        URL imgURL = ImageHelper.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
 
-	public static BufferedImage getBufferedImage(String path) {
-		URL imgURL = ImageHelper.class.getResource(path);
-		if (imgURL != null) {
-			try {
-				return ImageIO.read(imgURL);
-			} catch (IOException e) {
-				System.err.println("Couldn't find file: " + path);
-				return null;
-			}
-		} else {
-			System.err.println("Couldn't find file: " + path);
-			return null;
-		}
-	}
+    public static BufferedImage getBufferedImage(String path) {
+        URL imgURL = ImageHelper.class.getResource(path);
+        if (imgURL != null) {
+            try {
+                return ImageIO.read(imgURL);
+            } catch (IOException e) {
+                System.err.println("Couldn't find file: " + path);
+                return null;
+            }
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
 
-	public static final ImageIcon ICON_LOGO = createImageIcon("128/kugar.png");
-	public static final ImageIcon ICON_ADD = createImageIcon("32/add.png");
-	public static final ImageIcon ICON_SAVE = createImageIcon("32/save.png");
-	public static final ImageIcon ICON_EDIT = createImageIcon("32/edit.png");
-	public static final ImageIcon ICON_DELETE = createImageIcon("32/delete.png");
-	public static final ImageIcon ICON_SETTING = createImageIcon("32/setting.png");
-	public static final ImageIcon ICON_BACKUP = createImageIcon("32/backup.png");
-	public static final ImageIcon ICON_EXIT = createImageIcon("32/exit.png");
+    public static final ImageIcon ICON_LOGO = createImageIcon("128/kugar.png");
+    public static final ImageIcon ICON_SEARCH = createImageIcon("search.png");
 
-	public static final ImageIcon ICON_SEARCH = createImageIcon("search.png");
+    public static final ImageIcon ICON_ADD = createImageIcon("32/add.png");
+    public static final ImageIcon ICON_SAVE = createImageIcon("32/save.png");
+    public static final ImageIcon ICON_DELETE = createImageIcon("32/delete.png");
+    public static final ImageIcon ICON_SETTING = createImageIcon("32/setting.png");
+    public static final ImageIcon ICON_BACKUP = createImageIcon("32/backup.png");
+    public static final ImageIcon ICON_EXIT = createImageIcon("32/exit.png");
 }
