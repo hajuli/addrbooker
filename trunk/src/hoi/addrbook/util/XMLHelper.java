27f6cdac5b37801ffc6da9993965b62c910549ad
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-03-14 11:48:59 +0000

    update

--- trunk/src/hoi/addrbook/util/XMLHelper.java
+++ trunk/src/hoi/addrbook/util/XMLHelper.java
@@ -12,21 +12,16 @@ public class XMLHelper {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Properties xml = new Properties();
-        xml.put("NAME", "姓名");
-        xml.put("CLASSIFY", "分组");
-        xml.put("BIRTHDAY_LUNAR", "生日_农历");
-        xml.put("BIRTHDAY_SOLAR", "生日_阳历");
-        xml.put("TIMER", "计时");
-        xml.put("QQ", "QQ");
-        xml.put("MSN", "MSN");
-        xml.put("MOBILE", "手机");
-        xml.put("FETION", "飞信");
-        xml.put("EMAIL", "电子邮箱");
-        xml.put("WEBSITE", "个人主页");
-        xml.put("HOMEADDR", "家庭住址");
-        xml.put("COMPANY", "公司信息");
-        xml.put("NOTES", "附注");
-        xml.put("AGE", "年龄");
+        xml.put("AddrBooker", "草根通讯录");
+        xml.put("Backup data", "数据备份");
+        xml.put("Restore data", "数据恢复");
+        xml.put("Help", "帮助");
+        xml.put("Close", "关闭");
+        xml.put("Backup and Restore you data", "备份/恢复 数据");
+        xml.put("Settings", "选项 设置");
+        xml.put("Add Password", "设置密码");
+        xml.put("Change Password", "更改密码");
+        xml.put("Donate", "捐赠");
         xml.storeToXML(new FileOutputStream(new File("localize.xml")), Version.HOME_WEBSITE);
     }
 
