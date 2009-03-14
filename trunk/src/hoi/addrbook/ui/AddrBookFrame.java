7ab2bfd071a427bf16b98316fc38e0f6c469eb32
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-03-14 11:24:05 +0000

    AddrBookFrame

--- trunk/src/hoi/addrbook/ui/AddrBookFrame.java
+++ trunk/src/hoi/addrbook/ui/AddrBookFrame.java
@@ -3,6 +3,7 @@ package hoi.addrbook.ui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
+import hoi.addrbook.Version;
 import hoi.addrbook.icon.ImageHelper;
 
 import javax.swing.JFrame;
@@ -10,32 +11,32 @@ import javax.swing.SwingUtilities;
 
 public class AddrBookFrame extends JFrame {
 
-	private static final long serialVersionUID = -3116155141455555134L;
-	public static final String TITLE = "草根通讯录";
+    private static final long serialVersionUID = -3116155141455555134L;
+    public static final String TITLE = String.format("%s v%s", UINamesCtrl.getLocalName("AddrBooker"), Version.SHORT_VERSION);
 
-	public AddrBookFrame() {
-		Dimension size = new Dimension(540, 540 / 4 * 3);
-		setPreferredSize(size);
-		setMinimumSize(size);
-		setLocationRelativeTo(null);
-		setIconImage(ImageHelper.ICON_LOGO.getImage());
-		setTitle(TITLE);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		try {
-			SwingUtilities.invokeAndWait(new Runnable() {
-				public void run() {
-					setLayout(new BorderLayout(0, 0));
-					add(new AddrBookPanel(), BorderLayout.CENTER);
-				}
-			});
-		} catch (Exception exc) {
-			System.err.println("Can't create because of " + exc);
-			exc.printStackTrace();
-		}
-		setResizable(false);
-	}
+    public AddrBookFrame() {
+        Dimension size = new Dimension(540, 540 / 4 * 3);
+        setPreferredSize(size);
+        setMinimumSize(size);
+        setLocationRelativeTo(null);
+        setIconImage(ImageHelper.ICON_LOGO.getImage());
+        setTitle(TITLE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    setLayout(new BorderLayout(0, 0));
+                    add(new AddrBookPanel(), BorderLayout.CENTER);
+                }
+            });
+        } catch (Exception exc) {
+            System.err.println("Can't create because of " + exc);
+            exc.printStackTrace();
+        }
+        setResizable(false);
+    }
 
-	public static void main(String[] args) {
-		new AddrBookFrame().setVisible(true);
-	}
+    public static void main(String[] args) {
+        new AddrBookFrame().setVisible(true);
+    }
 }
