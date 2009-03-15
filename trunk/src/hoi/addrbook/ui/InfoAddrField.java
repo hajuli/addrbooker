1b758c813a1f3a4e186b102b0fce3c6e3404ae9f
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-03-15 17:24:19 +0000

    private static final String LS = "|";

--- trunk/src/hoi/addrbook/ui/InfoAddrField.java
+++ trunk/src/hoi/addrbook/ui/InfoAddrField.java
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
 public class InfoAddrField extends JPanel implements AccessInterface {
 
     private static final long serialVersionUID = 9087341622805152794L;
-    private static final char LS = '|';
+    private static final String LS = "|";
     private InfoTextField address = new InfoTextField();
     private InfoTextField postcode = new InfoTextField(null, null, UINamesCtrl.getLocalName("Postcode"));
     private String contactKey = null;
@@ -27,27 +27,23 @@ public class InfoAddrField extends JPanel implements AccessInterface {
         return contactKey;
     }
 
-    private String quote(String str) {
-        return str.replace(LS + "", String.format("\\%d", (int) LS));
-    }
-
-    private String unquote(String str) {
-        return str.replace(String.format("\\%d", (int) LS), LS + "");
-    }
-
     public String getContent() {
-        return address.getText().trim() + LS + quote(postcode.getText().trim());
+        return address.getText() + LS + postcode.getText();
     }
 
     public void setContent(String content) {
         if (content != null) {
             int k = content.lastIndexOf(LS);
-            if (k != -1) {
-                address.setText(content.substring(0, k).replace(LS + "", " ").trim());
-                postcode.setText(unquote(content.substring(k).replace(LS + "", " ").trim()));
+            if (k != -1) { // 可能识别错误，但是不会造成信息丢失
+                address.setText(content.substring(0, k));
+                postcode.setText(content.substring(k + LS.length()));
             } else {
-                address.setText(content.replace(LS + "", " ").trim());
+                address.setText(content);
+                postcode.setText("");
             }
+        } else {
+            address.setText("");
+            postcode.setText("");
         }
     }
 }
