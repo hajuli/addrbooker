4b8d8da5b8f4f49e7a76e70747102c22e7482dc2
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-03-15 08:08:43 +0000

    backup

--- trunk/src/hoi/addrbook/util/XMLHelper.java
+++ trunk/src/hoi/addrbook/util/XMLHelper.java
@@ -12,16 +12,23 @@ public class XMLHelper {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Properties xml = new Properties();
+
         xml.put("AddrBooker", "草根通讯录");
+
         xml.put("Backup data", "数据备份");
         xml.put("Restore data", "数据恢复");
         xml.put("Help", "帮助");
         xml.put("Close", "关闭");
         xml.put("Backup and Restore you data", "备份/恢复 数据");
+
         xml.put("Settings", "选项 设置");
-        xml.put("Add Password", "设置密码");
+        xml.put("Add Password", "创建密码");
         xml.put("Change Password", "更改密码");
         xml.put("Donate", "捐赠");
+
+        xml.put("Version", "版本");
+        xml.put("Found New Version", "发现新版本");
+
         xml.storeToXML(new FileOutputStream(new File("localize.xml")), VersionCtrl.HOME_WEBSITE);
     }
 
