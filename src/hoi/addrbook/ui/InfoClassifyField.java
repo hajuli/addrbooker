163a4af9f0e7680b80a45b1519708d08993f1770
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-03-18 15:30:54 +0000

    InfoClassifyField

--- src/hoi/addrbook/ui/InfoClassifyField.java
+++ src/hoi/addrbook/ui/InfoClassifyField.java
@@ -1,6 +1,8 @@
 package hoi.addrbook.ui;
 
 import java.awt.BorderLayout;
+import java.util.Vector;
+
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
@@ -8,13 +10,14 @@ import javax.swing.JPanel;
 public class InfoClassifyField extends JPanel implements AccessInterface {
 
     private static final long serialVersionUID = 7531465834540410473L;
-    private JComboBox classify = new JComboBox();
+    private JComboBox classify = null;
     private String contactKey = null;
 
-    public InfoClassifyField(String contactKey, String compName) {
+    public InfoClassifyField(String contactKey, String compName, Vector<String> classifys) {
         super(new BorderLayout(0, 0));
         this.contactKey = contactKey;
         setName(compName);
+        classify = new JComboBox(classifys);
         classify.setBorder(BorderFactory.createEtchedBorder());
         classify.setUI(new hoi.addrbook.look.AComboBoxUI());
         classify.setRenderer(new ComplexCellRenderer());
@@ -25,18 +28,12 @@ public class InfoClassifyField extends JPanel implements AccessInterface {
     public String getContactKey() {
         return contactKey;
     }
-    
+
     public String getContent() {
-        return "";
-    }
-
-    public void setContactKey(String contactKey) {
-        // TODO Auto-generated method stub
-
+        return classify.getSelectedItem().toString();
     }
 
     public void setContent(String content) {
-        // TODO Auto-generated method stub
-
+        classify.setSelectedItem(content);
     }
 }
