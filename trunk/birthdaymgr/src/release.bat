115a7b56b0d18a0bf7e80e321bd7a475de0cfa1f
haihoing <haihoing@c8e49d3c-f5ee-11dd-9d1a-b59b2e1864b6>
2009-04-06 02:19:53 +0000

    bacdkup

--- trunk/birthdaymgr/src/release.bat
+++ trunk/birthdaymgr/src/release.bat
@@ -1,1 +1,4 @@
+copy /V /Y /B TrayIcon12.dll %SystemRoot%\SYSTEM\
+copy /V /Y /B JIntellitype.dll %SystemRoot%\SYSTEM\
 jar cvfm birthdaymgr%date:~0,4%%date:~5,2%%date:~8,2%.jar manifest.mf hoi
+pause
